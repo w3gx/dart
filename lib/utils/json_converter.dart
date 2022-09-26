c057495e13d5cb69cd3af470aca7d3ad025f044f
elcharitas <jonathanirhodia@gmail.com>
2022-09-26 20:59:45 +0100

    feat: add utils to convert json

---
+++ lib/utils/json_converter.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+/// json object to string
+String stringify(dynamic items) {
+  if (items is List) {
+    return items.map(stringify).join(',');
+  } else if (items is Map) {
+    return items.keys
+        .map((key) => '${stringify(key)}:${stringify(items[key])}')
+        .join(',');
+  } else if (items is String) {
+    return '"${items.replaceAll('"', '\\"')}"';
+  }
+  return items.toString();
+}
+
+Map<String, dynamic> parse(String jsonString) {
+  return json.decode(jsonString);
+}
